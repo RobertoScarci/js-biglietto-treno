99e44ec03f7ab41caab3aa0819ae43d28b3e7387
RobertoScarci <robertoscarci.02@gmail.com>
2023-11-10 14:13:43 +0100

    inserimento dei primi dati nello script

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+// Chiedo all'utente i km che dovrà percorrere 
+
+const numberKm = prompt('Type the distance you want to travel in Km')
+
+// chiedo all'utente l'età personale
+
+const userAge = prompt('Type your age')
